0b9bdda4853ee655662c8cf2f89de344d528dcc5
Aditya Raj <ar25200312@gmail.com>
2026-07-13 21:03:34 +0530

    Time: 4 ms (4.4%), Space: 44 MB (10.81%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,30 @@
+class Solution {
+    public void sortColors(int[] arr) {
+        Arrays.sort(arr);
+    //     int n = arr.length;
+    //     int mid=0;
+    //     int hi =n-1;
+    //     int lo =0;
+    //       while(mid<=hi){
+    //      if(arr[mid]==2){
+            
+    //         int temp = arr[mid];
+    //         arr[mid] = arr[hi];
+    //         arr[hi] = temp;
+    //         hi--;
+            
+    //        }
+    //       else  if(arr[mid]==0){
+    //         int temp = arr[lo];
+    //         arr[lo] = arr[mid];
+    //         arr[mid] = temp;
+    //         mid++;
+    //         lo++;
+            
+    //        }
+    //      else  {
+    //         mid++;
+    //        }
+    // }
+    }
+}
